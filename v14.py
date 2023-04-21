a475d9e1209c055fffa66b2a3281883ff4cb2ff8
ManiT255 <41876462+ManiT255@users.noreply.github.com>
2023-04-21 10:24:49 -0500

    Update v14.py

--- v14.py
+++ v14.py
@@ -1,28 +1,78 @@
-import string
-import subprocess
-import time
+#WIP
 import itertools
-#Having to run the application via command line in Linux and supply the actual password it asks for.  Subprocess allows it to run in conjunction with the script
-def run_vault(password):
-    command = "./vault.o " + password
-    result = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
-    stdout, stderr = result.communicate()
-    return stdout.decode().strip()
-#As we are told the password is all lowercase this allows it to grow in length and ad a letter to each attemptNot fast by any means.  
-def generate_passwords(length):
-    for password in itertools.product(string.ascii_lowercase, repeat=length):
-        yield "".join(password)
-#So each password that is given is then tried and we are waiting for a response of "Success" while ignoring "Wrong password" and just attempting the next try.  This also waits for a response when the password is success so it just doesn't close on you while it ticks away.
-if __name__ == '__main__':
-    length = 1
+import random
+import string
+import timeit
+
+import numpy as np
+
+allowed_chars = string.ascii_lowercase + " "
+
+# read vault.o contents
+with open('vault.o', 'rb') as f:
+    vault_contents = f.read()
+
+def check_password(guess):
+    # test guess against the contents of vault.o
+    return guess.encode('ascii') in vault_contents
+
+def random_str(size):
+    return ''.join(random.choices(allowed_chars, k=size))
+
+def crack_length(max_len=32, verbose=False) -> int:
+    trials = 2000
+    times = np.empty(max_len)
+    for i in range(max_len):
+        i_time = timeit.repeat(stmt='check_password(x)',
+                               setup=f'x=random_str({i!r})',
+                               globals=globals(),
+                               number=trials,
+                               repeat=10)
+        times[i] = min(i_time)
+
+    if verbose:
+        most_likely_n = np.argsort(times)[::-1][:5]
+        print(most_likely_n, times[most_likely_n] / times[most_likely_n[0]])
+
+    most_likely = int(np.argmax(times))
+    return most_likely
+
+def crack_password(length, verbose=False):
+    guess = random_str(length)
+    counter = itertools.count()
+    trials = 1000
     while True:
-        passwords = generate_passwords(length)
-        for password in passwords:
-            result = run_vault(password)
-            print(f"Trying password: {password}")
-            if result == "Success":
-                print(f"Success! Password is {password}")
-                input("Press Enter to exit")
-                quit()
-            time.sleep(1)
-        length += 1
+        i = next(counter) % length
+        for c in allowed_chars:
+            alt = guess[:i] + c + guess[i + 1:]
+
+            alt_time = timeit.repeat(stmt='check_password(x)',
+                                     setup=f'x={alt!r}',
+                                     globals=globals(),
+                                     number=trials,
+                                     repeat=10)
+            guess_time = timeit.repeat(stmt='check_password(x)',
+                                       setup=f'x={guess!r}',
+                                       globals=globals(),
+                                       number=trials,
+                                       repeat=10)
+
+            if check_password(alt):
+                return alt
+
+            if min(alt_time) > min(guess_time):
+                guess = alt
+                if verbose:
+                    print(guess)
+
+
+def main():
+    length = crack_length(verbose=True)
+    print(f"using most likely length {length}")
+    input("hit enter to continue...")
+    password = crack_password(length, verbose=True)
+    print(f"password cracked:'{password}'")
+
+
+if __name__ == '__main__':
+    main()
